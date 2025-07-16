dc77c825dbdb8b724ba4bd15c54ad5eb2a8f5894
NK-daCoder <gpnkstudios@gmail.com>
2025-07-16 08:47:58 +0200

    fixed form submitted to the backend

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 const AuthForm = ({ type }) => {
   const [formData, setFormData] = useState({ username: '', email: '', password: '', password2: '' });
@@ -26,7 +24,7 @@ const AuthForm = ({ type }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const url = `${process.env.BACKEND_LOCAL_URL}${endpointMap[type]}`;
+    const url = `http://127.0.0.1:8000${endpointMap[type]}`;
 
     const payload = isLogin
       ? { username: formData.username, password: formData.password }
@@ -42,6 +40,7 @@ const AuthForm = ({ type }) => {
       if (!res.ok) throw new Error('Failed to authenticate');
 
       const data = await res.json();
+      console.log(data)
 
       // If login, handle tokens and redirect
       if (isLogin) {
@@ -49,11 +48,16 @@ const AuthForm = ({ type }) => {
         localStorage.setItem('refresh_token', data.refresh);
         localStorage.setItem('user', JSON.stringify(data.user));
 
-        if (data.user.is_admin) navigate('/admin/dashboard');
-        else if (data.user.is_agent) navigate('/agent/dashboard');
-        else navigate('/register');
+        if (!data.user.is_profile_complete) {
+          navigate('/complete-profile');
+        } else if (data.user.is_admin) {
+          navigate('/admin/dashboard');
+        } else if (data.user.is_agent) {
+          navigate('/agent/dashboard');
+        }
+
       } else {
-        navigate('/complete-profile');
+        navigate('/');
       }
     } catch (err) {
       alert(err.message);
@@ -65,27 +69,27 @@ const AuthForm = ({ type }) => {
       <form onSubmit={handleSubmit} className='bg-white p-6 rounded-2xl border-t border-stone-200 shadow-md flex flex-col gap-4 xl:w-[50%] lg:w-[60%] md:w-[70%] sm:w-[80%] w-full'>
         <fieldset className="flex flex-col">
           <legend className="text-2xl mb-4 flex items-center gap-2 tracking-wide text-blue-500 font-medium font-sans">
-            <span className="size-12 bg-gradient-to-b from-blue-500 to-blue-600 text-white rounded-2xl shadow-lg shadow-blue-300 border-t border-blue-300">
+            <span className="size-12 bg-gradient-to-b from-blue-500 to-blue-600 text-white rounded-2xl shadow-md shadow-blue-600/50 border-t border-blue-300">
               <i className="fi fi-rr-user-pen size-full flex items-center justify-center pl-1" aria-hidden={true}></i>
             </span>
             {isLogin ? 'Login' : isAgent ? 'Register as Agent' : 'Register as Admin'}
           </legend>
           <label htmlFor="username" className="text-sm tracking-wide mt-5 mb-2 text-stone-600 flex items-center gap-2">
-            <i class="fi fi-rr-user flex items-center justify-center" aria-label={true}></i>
+            <i className="fi fi-rr-user flex items-center justify-center" aria-label={true}></i>
             Username
           </label>
           <input type="text" id="username" name="username" placeholder="Username" required onChange={handleChange} className="text-sm border rounded-2xl px-3 py-2"/>
           {!isLogin && (
             <React.Fragment>
             <label htmlFor="email" className="text-sm tracking-wide mt-5 mb-2 text-stone-600 flex items-center gap-2">
-              <i class="fi fi-rr-envelope flex items-center justify-center" aria-label={true}></i>
+              <i className="fi fi-rr-envelope flex items-center justify-center" aria-label={true}></i>
               Email
             </label>
             <input type="email" id="email" name="email" placeholder="Email" required onChange={handleChange} className="text-sm border rounded-2xl px-3 py-2"/>
             </React.Fragment>
           )}
           <label htmlFor="password" className="text-sm tracking-wide mt-5 mb-2 text-stone-600 flex items-center gap-2">
-            <i class="fi fi-rr-lock flex items-center justify-center" aria-label={true}></i>
+            <i className="fi fi-rr-lock flex items-center justify-center" aria-label={true}></i>
             Password
           </label>
           <div className='relative'>
@@ -97,7 +101,7 @@ const AuthForm = ({ type }) => {
           {!isLogin && (
             <React.Fragment>
               <label htmlFor="password2" className="text-sm tracking-wide mt-5 mb-2 text-stone-600 flex items-center gap-2">
-                <i class={`fi fi-rr-lock`} aria-label={true}></i>
+                <i className={`fi fi-rr-lock`} aria-label={true}></i>
                 Retype Password
               </label>
               <div className='relative'>
@@ -111,7 +115,7 @@ const AuthForm = ({ type }) => {
         </fieldset>
         
 
-        <button type="submit" className="bg-gradient-to-b from-blue-500 to-blue-600 py-3 rounded-full text-white font-medium tracking-wide shadow-md shadow-blue-300 mt-5 transition-transform hover:scale-[1.01] ">{isLogin ? 'Continue' : 'Register'}</button>
+        <button type="submit" className="border-t-2 border-blue-400 bg-gradient-to-b from-blue-500 to-blue-600 py-3 rounded-full text-white font-medium tracking-wide shadow-lg shadow-blue-500 mt-5 transition-transform hover:scale-[1.01] ">{isLogin ? 'Continue' : 'Register'}</button>
       </form>
     </div>
   );
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+      first_name: '',
+      last_name: '',
+      phone_number: '',
+      address: '',
+      race: '',
+      bio: '',
+      profile_image: null,
+    });
+
+    const handleChange = (e) => {
+      const { name, value, files } = e.target;
+      setFormData((prev) => ({
+        ...prev,
+        [name]: files ? files[0] : value,
+      }));
+    };
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const token = localStorage.getItem('access_token');
+      const body = new FormData();
+
+      for (const key in formData) {
+        body.append(key, formData[key]);
+      }
+
+      const res = await fetch('http://127.0.0.1:8000/api/users/me/profile/', {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body,
+      });
+
+      if (res.ok) {
+        navigate('/agent/dashboard'); // or /admin/dashboard
+      } else {
+        alert('Profile update failed');
+      }
+    };
+
   return (
-    <div>Profile</div>
+    <div className="bg-stone-200 flex flex-col justify-center items-center min-h-screen">
+      <form onSubmit={handleSubmit} className="bg-white p-4 rounded-2xl lg:w-[60%] md:w-[70%] sm:w-[80%] w-full shadow-md flex flex-col gap-4 overflow-y-auto max-h-[35rem]">
+        <fieldset className="flex flex-col">
+          <legend className="p-4 text-center tracking-wide text-xl flex flex-col items-center justify-center gap-2 text-blue-500 font-medium font-sans">
+            <span aria-hidden={true} className='size-12 text-white bg-gradient-to-b from-blue-500 to-blue-600 rounded-2xl shadow-md shadow-blue-600/50 border-t border-blue-300'>
+              <i className="fi fi-rr-user-pen size-full flex items-center justify-center pl-1" aria-hidden={true}></i>
+            </span>
+            Setup Your Profile
+          </legend>
+
+          <label htmlFor="first-name" className="text-sm tracking-wide mt-5 mb-1 flex gap-2">
+            First Name:
+          </label>
+          <input name="first_name" type="text" id="first-name" placeholder="First Name" required onChange={handleChange} className="text-sm border rounded-2xl px-4 py-3"/>
+
+          <label htmlFor="last-name" className="text-sm tracking-wide mt-5 mb-1 flex gap-2">
+            Last name:
+          </label>
+          <input type="text" name="last_name" id="last-name" placeholder="Last Name" required onChange={handleChange} className="text-sm border rounded-2xl px-4 py-3"/>
+
+          <label htmlFor="phone-number" className="text-sm tracking-wide mt-5 mb-1 flex gap-2">
+            Tel:
+          </label>
+          <input type="text" id="phone-number" name="phone_number" onChange={handleChange} className="text-sm border rounded-2xl px-4 py-3"/>
+
+          <label htmlFor="address" className="text-sm tracking-wide mt-5 mb-1 flex gap-2">
+            Address:
+          </label>
+          <input type="text" id="address" name="address" onChange={handleChange} className="text-sm border rounded-2xl px-4 py-3"/>
+
+          <label htmlFor="bio" className="text-sm tracking-wide mt-5 mb-1 flex gap-2">
+            Biography:
+          </label>
+          <textarea id="bio" name='bio' placeholder="Bio" onChange={handleChange} className="text-sm border rounded-2xl px-4 py-3 h-40"/>
+
+          <label htmlFor="image-uploader" className='mt-5 mb-1 flex gap-2'>
+            Profile Image
+          </label>
+          <input type="file" id="image-upoader" name="profile_image" onChange={handleChange} className="text-sm border rounded-2xl px-4 py-3"/>
+          <label htmlFor="race" className="">Race:</label>
+          <input id="race" type="text" className="text-sm border rounded-2xl" name="race" onChange={handleChange}/>
+          <button type="submit" className="mt-5 p-3 bg-gradient-to-b text-white from-blue-500 to-blue-700 rounded-full tracking-wide font-medium shadow-md shadow-blue-700/30">Save Profile</button>
+        </fieldset>
+      </form>
+    </div>
   )
 }
 
